a25d43f0c81fc76f572e1d7bdc782a0727102e36
Diana Le <lepdiana01@gmail.com>
2019-08-22 13:32:11 -0600

    if statement olympics added

---
+++ exercises/if-statement-olympics/app.js
@@ -0,0 +1,48 @@
+// if (5 > 3) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+// var animal = "cat";
+// if (animal.length === 3) {
+//     console.log("is the length")
+// } else {
+//     console.log("is not the length")
+// }
+
+// if ("cat" !== "dog") {
+//     console.log("not the same")
+// } else {
+//     console.log("the same")
+// }
+
+// var person = {
+//     name: "Bobby",
+//     age: 12
+//   }
+// if (person.age >= 18) {
+//     console.log(person.name + " is allowed to go to the movie")
+// } else {
+//     console.log (person.name + " is not allowed to go to the movie")
+// }
+
+// if (1 === "1") {
+//     console.log("strict")
+// } else if (1 == "1") {
+//     console.log("abstract")
+// } else {
+//     console.log("not equal at all")
+// }
+
+// let animal = "dog";
+//     if (animal === "dog") {
+//         console.log("string")
+//     }
+
+// let dataType = "true"; 
+// if (dataType === "true") {
+//     console.log("not a boolean")
+// } else {
+//     console.log("boolean")
+// }
