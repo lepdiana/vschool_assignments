1d56e173cea19fc1aa82a3620c85a107fb07c3a8
Diana Le <lepdiana01@gmail.com>
2019-08-22 16:51:11 -0600

    added for loop olympics

--- exercises/for-loop-olympics/app.js
+++ exercises/for-loop-olympics/app.js
@@ -11,3 +11,18 @@
 //     console.log(fruit[i])
 // }
 
+// for (i = 0; i <= 100; i ++) {
+//     if(i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// var people = ["Riley", "Monica", "Chandler", "Rachel"];
+// for (var i = 0; i < 4; i++) {
+//     console.log(people[i]);
+// }
+
+// var zero = [0, 0, 0];
+// for (var i = 0; i < 3; i++) {
+//     console.log(zero)
+// }
