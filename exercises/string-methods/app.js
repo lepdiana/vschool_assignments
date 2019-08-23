182e6249043db86c6b0111b9e1f4ac8c95c6d776
Diana Le <lepdiana01@gmail.com>
2019-08-23 16:44:16 -0600

    string methods

--- exercises/string-methods/app.js
+++ exercises/string-methods/app.js
@@ -1,6 +1,37 @@
-// var word = 'hello';
-// var upWord = word.toUpperCase();
-// var lowWord = word.toLowerCase ();
-// console.log(upWord + lowWord);
+// function character (word) {
+//     let upWord = word.toUpperCase();
+//     let lowWord = word.toLowerCase ();
+//     console.log(upWord + lowWord)
+// }
 
-const findMiddleIndex = "example";
+// character ("welcome");
+
+
+
+// function half (word) {
+//     const stringLength = word.length;
+//     // console.log(stringLength);
+//     const halfLength = stringLength / 2;
+//     // console.log(halfLength);
+//     return Math.floor(halfLength)
+// }
+
+// console.log(half ("hello"));
+
+// function example (word) {
+//     const stringLength = word.length;
+//     const returnFirstHalf = stringLength / 2;
+//     return word.slice(0, Math.floor(returnFirstHalf))
+// }
+
+// console.log(example ("whatever"));
+
+
+function lastProblem (todo) {
+    const firstHalf = todo.slice(0, todo.length / 2).toUpperCase();
+    const secondHalf = todo.slice(todo.length / 2, todo.length).toLowerCase();
+    const combine = firstHalf.concat(secondHalf);
+    return combine
+}
+
+console.log (lastProblem ("donedonedonedone"));
