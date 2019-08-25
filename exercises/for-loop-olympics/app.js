73a148a25135f94d41843c8e5e11a8208ffcfa2c
Diana Le <lepdiana01@gmail.com>
2019-08-25 14:08:21 -0600

    finished loop olympics

--- exercises/for-loop-olympics/app.js
+++ exercises/for-loop-olympics/app.js
@@ -11,18 +11,73 @@
 //     console.log(fruit[i])
 // }
 
+// var numbers = [];
+// for (var i = 0; i <= 9; i++) {
+//     numbers.push(i)
+// }
+
+// console.log(numbers);
+
+
 // for (i = 0; i <= 100; i ++) {
 //     if(i % 2 === 0) {
 //         console.log(i)
 //     }
 // }
 
-// var people = ["Riley", "Monica", "Chandler", "Rachel"];
-// for (var i = 0; i < 4; i++) {
-//     console.log(people[i]);
+// var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+// for (var i = 0; i < fruit.length; i += 2){
+//     console.log(fruit[i])
 // }
 
-// var zero = [0, 0, 0];
+// var peopleArray = [
+//     {
+//       name: "Harrison Ford",
+//       occupation: "Actor"
+//     },
+//     {
+//       name: "Justin Bieber",
+//       occupation: "Singer"
+//     },
+//     {
+//       name: "Vladimir Putin",
+//       occupation: "Politician"
+//     },
+//     {
+//       name: "Oprah",
+//       occupation: "Entertainer"
+//     }
+//   ]
+
+//   for (var i = 0; i < peopleArray.length; i++) {
+//       console.log(peopleArray[i].name)
+//   }
+
+// var peopleName = [];
+// var peopleOccupation = [];
+// for (var i = 0; i < peopleArray.length; i++) {
+//     peopleName.push(peopleArray[i].name);
+//     peopleOccupation.push(peopleArray[i].occupation)
+// }
+// console.log(peopleName);
+// console.log(peopleOccupation)
+
+// var peopleName = [];
+// var peopleOccupation = [];
+// for (var i = 0; i < peopleArray.length; i += 2) {
+//     peopleName.push(peopleArray[i].name);
+//     peopleOccupation.push(peopleArray[i].occupation)
+// }
+// console.log(peopleName);
+// console.log(peopleOccupation)
+
+// var arrZero = [0, 0, 0];
 // for (var i = 0; i < 3; i++) {
-//     console.log(zero)
+//     console.log(arrZero)
+// }
+
+
+// var arr = [0, 1, 2];
+// for (var i = 0; i < 3; i++) {
+//     console.log(arr)
 // }
