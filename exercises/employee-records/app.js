cf03b54d105f989682b6e12fb0dd5fdd3af0e39b
Diana Le <lepdiana01@gmail.com>
2019-09-04 10:08:59 -0600

    revised employee

--- exercises/employee-records/app.js
+++ exercises/employee-records/app.js
@@ -1,32 +1,30 @@
 const employees = []
 
-function Employee(name, jobTitle, salary, status="Full time"){
+function Employee(name, jobTitle, salary){
     this.name = name;
     this.jobTitle = jobTitle;
     this.salary = salary;
-    this.status = status;
-    this.printEmployeeForm = function(){
-        console.log(`
-        Name: ${this.name}
-        Job title: ${this.jobTitle}
-        Salary: ${this.salary}
-        Status: ${this.status}
-        `);
-    }
+    this.status = "Full time";
 }
 
-const employee1 = new Employee("Susan", "Mechanical Engineer", 120000, "Part time")
-const employee2 = new Employee("Robert", "Data Analyst", 58000, "Contract")
+Employee.prototype.printEmployeeForm = function(){
+    return this
+}
+
+const employee1 = new Employee("Susan", "Mechanical Engineer", 120000)
+employee1.status = "Part time"
+console.log(employee1.printEmployeeForm())
+const employee2 = new Employee("Robert", "Data Analyst", 58000)
+employee2.status = "Contract"
+console.log(employee2.printEmployeeForm())
 const employee3 = new Employee("Jake", "Auto Mechanic", 38000, "Part time")
+employee3.status = "Part time"
+console.log(employee3.printEmployeeForm())
 
 employees.push(employee1)
 employees.push(employee2)
 employees.push(employee3)
 // console.log(employees)
-
-console.log(employee1.printEmployeeForm())
-console.log(employee2.printEmployeeForm())
-console.log(employee3.printEmployeeForm())
 
 
 
