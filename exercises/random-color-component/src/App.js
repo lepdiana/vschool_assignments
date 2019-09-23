75b5590a6999df4007968ef9d954ccce8547a338
Diana Le <lepdiana01@gmail.com>
2019-09-23 17:11:59 -0600

    finished random color component

---
+++ exercises/random-color-component/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      colors: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://www.colr.org/json/colors/random/7')
+    .then((response)=>{
+      this.setState({colors: response.data.colors})
+    })
+  }
+
+  render() {
+    const mappedColors = this.state.colors.map(color => {
+      return <h1 style={{height: 200, width: 200, backgroundColor: `#${color.hex}`}}>{color.hex}</h1>
+    })
+    return(
+      <div>
+        { mappedColors }
+      </div>
+    )
+  }
+}
+
+export default App;
