10eb8f1fc6ea4f7e1e81ac77a84ba936226f8adf
Diana Le <lepdiana01@gmail.com>
2019-10-20 16:41:47 -0600

    moved portfolio

--- projects/personal-react-site/src/index.js
+++ projects/personal-react-site/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
-import './style/body.css'
+import App from './App.js'; 
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
